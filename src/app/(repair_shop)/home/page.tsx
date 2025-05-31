aa5fdd811049e800fe26d0ddff05fcb25c8c3138
NeilRow56 <neilrowland56@gmail.com>
2025-05-31 05:13:34 +0100

    Tweaks to error.jsx file

--- src/app/(repair_shop)/home/page.tsx
+++ src/app/(repair_shop)/home/page.tsx
@@ -1,4 +1,12 @@
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count)
+}
+
 const Home = () => {
+  const random = getRandomInt(2)
+  if (random === 1) {
+    throw new Error('Error loading review')
+  }
   return <div>Home page</div>
 }
 
--- src/app/(repair_shop)/layout.tsx
+++ src/app/(repair_shop)/layout.tsx
@@ -9,20 +9,9 @@ export default async function RepairShopLayout({
   children: React.ReactNode
 }) {
   return (
-    // <div className='mx-auto w-full max-w-7xl'>
-    //   <Header />
-    //   <div className='px-4 py-2'>{children}</div>
-    // </div>
     <SidebarProvider>
       <AdminSidebar />
       <SidebarInset>
-        {/* <header className='flex h-16 shrink-0 items-center gap-2 border-b px-4'>
-          <SidebarTrigger className='-ml-1' />
-          <Separator orientation='vertical' className='mr-2 h-4' />
-          <Breadcrumb className='text-primary hidden text-xl md:block'>
-            Your local library online!
-          </Breadcrumb>
-        </header> */}
         <div className='mx-auto w-full max-w-7xl items-center'>
           <Header />
         </div>
